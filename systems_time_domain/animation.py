b15e313a9e9ddb578323b86746a2cc222f297918
Sascha Spors <Sascha.Spors@uni-rostock.de>
2019-04-23 16:32:22 +0200

    Revised implementation of animation to use sympy directly, modified signal, minor edits of text

--- systems_time_domain/animation.py
+++ systems_time_domain/animation.py
@@ -1,10 +1,11 @@
-import numpy as np
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import sympy as sym
+from sympy.plotting.plot import MatplotlibBackend
+import numpy as np
 
 
-def animate_convolution(x, h, y, t, tau, td, taud):
+def animate_convolution(x, h, y, t, tau, td, taud, interval=75):
     """Plot animation of graphical representation of linear convolution.
 
     Parameters
@@ -19,55 +20,48 @@ def animate_convolution(x, h, y, t, tau, td, taud):
         Integration variable for convolution.
     td: array like
         Discrete values of independent variable evaluated for plot.
-    taud:
+    taud: array_like
         Discrete values of integration variable evaluated for animation.
+    interval: integer
+        Interval in ms between frames.
 
     Returns
     -------
     matplotlib.animation.FuncAnimation object
 
     """
-
     def heaviside(x):
         return np.heaviside(x, .5)
 
     def animate(ti):
-        ax[0].collections.clear()
-        ax[0].fill_between(taud, 0, h_eval(ti, taud)*x_eval(taud), facecolor='red', alpha=0.3)
+        ax[0].clear()
+        ax[0].fill_between(taud, 0, xh_eval(ti, taud), facecolor='red', alpha=0.3)
+        p1 = sym.plot(h.subs(t, t - tau).subs(t, ti), (tau, taud[0], taud[-1]), xlabel=r'$\tau$', ylabel=r'$h(t-\tau)$, $x(\tau)$', line_color='C0', show=False)
+        p1.append(sym.plot(x.subs(t, tau), (tau, taud[0], taud[-1]), line_color='C1', show=False)[0])
+        backend1 = MatplotlibBackend(p1)
+        backend1.ax = ax[0]
+        backend1.process_series()
+        plt.close(backend1.fig)
 
-        lines[0].set_data(taud, h_eval(ti, taud))
-        lines[1].set_data(taud, x_eval(taud))
-
-        lines[2].set_data(taud, y_eval(taud))
-        lines[3].set_data(ti, y_eval(ti))
-
-        return lines
+        ax[1].clear()
+        ax[1].plot(ti, y.subs(t, ti), 'ro')
+        p2 = sym.plot(y, (t, taud[0], taud[-1]), xlabel=r'$t$', ylabel=r'$y(t)$', line_color='C2', show=False)
+        backend2 = MatplotlibBackend(p2)
+        backend2.ax = ax[1]
+        backend2.process_series()
+        plt.close(backend2.fig)
 
     # add numerical evaluation to symbolic functions
-    x_eval = sym.lambdify((tau), x.subs(t, tau), modules=['numpy', {'Heaviside': heaviside}])
-    h_eval = sym.lambdify((t, tau), h.subs(t, t - tau), modules=['numpy', {'Heaviside': heaviside}])
-    y_eval = sym.lambdify((t), y, modules=['numpy', {'Heaviside': heaviside}])
+    xh_eval = sym.lambdify((t, tau), x.subs(t, tau) * h.subs(t, t - tau), modules=['numpy', {'Heaviside': heaviside}])
 
-    # setup plot and line styles
-    fig, ax = plt.subplots(2, 1)
-    fig.subplots_adjust(hspace=0.5)
-    plt.close()  # suppresses empty plot in notebook
+    # setup plot
+    fig, ax = plt.subplots(nrows=2)
 
-    lines = [ax[0].plot([], [], label=r'$h(t-\tau)$')[0]]
-    lines.append(ax[0].plot([], [], label=r'$x(\tau)$')[0])
-    lines.append(ax[1].plot([], [], 'g-', label=r'$y(t) = x(t) * h(t)$')[0])
-    lines.append(ax[1].plot([], [], 'ro')[0])
+    for axi in ax:
+        axi.spines['top'].set_color('none')
+        axi.spines['right'].set_color('none')
+        axi.spines['bottom'].set_position('zero')
 
-    ax[0].set_xlim((-3, 5))
-    ax[0].set_ylim((-.1, 1.2))
-    ax[0].set_xlabel(r'$\tau$')
-    ax[0].legend(loc='upper right')
-    ax[0].grid(True)
+    plt.close()  # suppress empty plot in notebook
 
-    ax[1].set_xlim((-3, 5))
-    ax[1].set_ylim((-.1, 1.2))
-    ax[1].set_xlabel(r'$t$')
-    ax[1].legend(loc='upper right')
-    ax[1].grid(True)
-
-    return animation.FuncAnimation(fig, animate, td, interval=50, blit=True)
+    return FuncAnimation(fig, animate, td, interval=interval)
