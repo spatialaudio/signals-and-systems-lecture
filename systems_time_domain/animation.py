9fd86c08856a650d8a16ae12d5f860ceb150a3ba
Sascha Spors <Sascha.Spors@uni-rostock.de>
2019-04-25 15:51:46 +0200

    Changed implementation of animation to use sympy plotting functionality with matplotlib collections

--- systems_time_domain/animation.py
+++ systems_time_domain/animation.py
@@ -1,13 +1,12 @@
-from matplotlib.animation import FuncAnimation
-import matplotlib.pyplot as plt
-import sympy as sym
-from sympy.plotting.plot import MatplotlibBackend
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.collections import LineCollection, PolyCollection
+import sympy as sym
 
 
 def animate_convolution(x, h, y, t, tau, td, taud, interval=75):
     """Plot animation of graphical representation of linear convolution.
-
     Parameters
     ----------
     x : sympy function
@@ -20,48 +19,64 @@ def animate_convolution(x, h, y, t, tau, td, taud, interval=75):
         Integration variable for convolution.
     td: array like
         Discrete values of independent variable evaluated for plot.
-    taud: array_like
+    taud:
         Discrete values of integration variable evaluated for animation.
-    interval: integer
-        Interval in ms between frames.
-
+    interval:
+        Interval in ms between frames of animation.
     Returns
     -------
     matplotlib.animation.FuncAnimation object
-
     """
-    def heaviside(x):
-        return np.heaviside(x, .5)
-
     def animate(ti):
-        ax[0].clear()
-        ax[0].fill_between(taud, 0, xh_eval(ti, taud), facecolor='red', alpha=0.3)
-        p1 = sym.plot(h.subs(t, t - tau).subs(t, ti), (tau, taud[0], taud[-1]), xlabel=r'$\tau$', ylabel=r'$h(t-\tau)$, $x(\tau)$', line_color='C0', show=False)
-        p1.append(sym.plot(x.subs(t, tau), (tau, taud[0], taud[-1]), line_color='C1', show=False)[0])
-        backend1 = MatplotlibBackend(p1)
-        backend1.ax = ax[0]
-        backend1.process_series()
-        plt.close(backend1.fig)
+        p = sym.plot(x.subs(t, tau), (tau, taud[0], taud[-1]), show=False)
+        line_x.set_segments(p[0].get_segments())
 
-        ax[1].clear()
-        ax[1].plot(ti, y.subs(t, ti), 'ro')
-        p2 = sym.plot(y, (t, taud[0], taud[-1]), xlabel=r'$t$', ylabel=r'$y(t)$', line_color='C2', show=False)
-        backend2 = MatplotlibBackend(p2)
-        backend2.ax = ax[1]
-        backend2.process_series()
-        plt.close(backend2.fig)
+        p = sym.plot(h.subs(t, t - tau).subs(t, ti), (tau, taud[0], taud[-1]), show=False)
+        line_h.set_segments(p[0].get_segments())
 
-    # add numerical evaluation to symbolic functions
-    xh_eval = sym.lambdify((t, tau), x.subs(t, tau) * h.subs(t, t - tau), modules=['numpy', {'Heaviside': heaviside}])
+        p = sym.plot(y, (t, taud[0], taud[-1]), show=False)
+        line_y.set_segments(p[0].get_segments())
 
-    # setup plot
-    fig, ax = plt.subplots(nrows=2)
+        p = sym.plot(x.subs(t, tau)*h.subs(t, ti - tau), (tau, -5, 5), show=False)
+        points = p[0].get_points()
+        verts = [[(xi[0], xi[1]) for xi in np.transpose(np.array(points))]]
+        fill.set_verts(verts)
 
-    for axi in ax:
-        axi.spines['top'].set_color('none')
-        axi.spines['right'].set_color('none')
-        axi.spines['bottom'].set_position('zero')
+        dot.set_data(ti, y.subs(t, ti))
 
-    plt.close()  # suppress empty plot in notebook
+    # setup plot and line/fill styles
+    fig, ax = plt.subplots(2, 1)
+    fig.subplots_adjust(hspace=0.5)
+    plt.close()  # suppresses empty plot in notebook
+
+    fill = PolyCollection([], facecolors='r')
+    fill.set_alpha(0.3)
+    ax[0].add_collection(fill)
+
+    line_x = LineCollection([], colors='C0')
+    line_x.set_label(r'$x(\tau)$')
+    ax[0].add_collection(line_x)
+
+    line_h = LineCollection([], colors='C1')
+    line_h.set_label(r'$h(t - \tau)$')
+    ax[0].add_collection(line_h)
+
+    line_y = LineCollection([], colors='C2')
+    line_y.set_label(r'$y(t)$')
+    ax[1].add_collection(line_y)
+
+    dot, = ax[1].plot([], 'ro')
+
+    ax[0].set_xlim((-2, 5))
+    ax[0].set_ylim((-.1, 1.2))
+    ax[0].set_xlabel(r'$\tau$')
+    ax[0].legend(loc='upper right')
+    ax[0].grid(True)
+
+    ax[1].set_xlim((-2, 5))
+    ax[1].set_ylim((-.1, 1.2))
+    ax[1].set_xlabel(r'$t$')
+    ax[1].legend(loc='upper right')
+    ax[1].grid(True)
 
     return FuncAnimation(fig, animate, td, interval=interval)
