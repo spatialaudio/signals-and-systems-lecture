559755ade81e7f58a1313ebbc9ebabffdc32afd1
Sascha Spors <Sascha.Spors@uni-rostock.de>
2019-07-04 12:30:04 +0200

    Finalized notebook on transfer function

--- ipython_kernel_config.py
+++ ipython_kernel_config.py
@@ -1,4 +1,3 @@
-#c.InlineBackend.figure_formats = {'svg', 'pdf'}
-c.InlineBackend.figure_formats = {'png', 'pdf'}
+c.InlineBackend.figure_formats = {'svg', 'pdf'}
 c.InlineBackend.rc = {'figure.dpi': 96}
 
